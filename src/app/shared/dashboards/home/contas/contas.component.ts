5f7a42471f73316306be8bee3895926f5cb1ef81
genildocs <genildocs@gmail.com>
2024-12-27 03:40:31 -0300

    commit

--- src/app/shared/dashboards/home/contas/contas.component.ts
+++ src/app/shared/dashboards/home/contas/contas.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ContaService } from '../../../services/conta.service';
 @Component({
   selector: 'app-contas',
   templateUrl: './contas.component.html',
   styleUrl: './contas.component.css',
 })
-export class ContasComponent {
+export class ContasComponent implements OnInit {
   visible: boolean = false;
+
+  constructor(private contaService: ContaService) {}
+
+  ngOnInit(): void {
+    this.showUser();
+  }
+
+  showUser() {
+    console.log('oi');
+    this.contaService.getUsers().subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 
   showDialog() {
     this.visible = true;
---
+++ src/app/shared/services/conta.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+@Injectable({
+  providedIn: 'root',
+})
+export class ContaService {
+  private apiUrl = 'https://api-gestor-x0c7.onrender.com/api/v1';
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private authService: AuthService
+  ) {}
+
+  getUsers() {
+    return this.http.get(`${this.apiUrl}/users`);
+  }
+}
