90c9e36975b99c60e5bff7463aa336a8b3117abe
genildocs <genildocs@gmail.com>
2025-01-06 06:37:04 -0300

    commit

--- src/app/shared/services/layout.service.ts
+++ src/app/shared/services/layout.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LayoutService {
+  private renderer: Renderer2;
+  private sidebarState = new BehaviorSubject<boolean>(false); //controla o estado da sidebar
+  constructor(rendererFactory: RendererFactory2) {
+    this.renderer = rendererFactory.createRenderer(null, null);
+  }
 
-  constructor() { }
+  toggleSidebar(element: HTMLElement): void {}
 }
