a5fbd14e9f15eae1f0e4916181c4230df0e2fd50
genildocs <genildocs@gmail.com>
2025-01-07 01:54:59 -0300

    refatoracao apexchart

--- src/app/shared/components/chart-contas/chart-contas.component.ts
+++ src/app/shared/components/chart-contas/chart-contas.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import {
   ChartComponent,
   ApexAxisChartSeries,
@@ -13,8 +19,6 @@ import {
   ApexTooltip,
   ApexFill,
 } from 'ng-apexcharts';
-import { ContaService } from '../../services/conta.service';
-import { addDays, format } from 'date-fns/fp';
 import { DateTime } from 'luxon';
 export type ChartOptions = {
   series: ApexAxisChartSeries;
@@ -37,192 +41,176 @@ export type ChartOptions = {
 export class ChartContasComponent implements OnInit {
   @ViewChild('chartObj') chart!: ChartComponent;
   public chartOptions: Partial<ChartOptions>;
-  dt: Date = DateTime.now().toJSDate();
   @Input() contas: any[] = [];
-  constructor(private contaService: ContaService) {
+  constructor() {
     this.chartOptions = {};
   }
 
-  ngOnInit(): void {
-    this.buscaContas();
-  }
-
-  filterContas(contas: any, select?: string): any {
-    const filters = contas.filter(
-      (conta: any) => conta.status === select || conta.tipo === select
-    );
-
-    if (select) {
-      const valores = filters.map((conta: any) => conta.valor);
-      const vencimentos = filters.map(
-        (conta: any) => DateTime.fromISO(conta.vencimento).monthShort
-      );
-
-      const totalPorMes = vencimentos.reduce(
-        (acc: any, mes: any, index: any) => {
-          if (!acc[mes]) {
-            acc[mes] = 0;
-          }
-
-          acc[mes] += Math.round(valores[index]);
-
-          return acc;
-        },
-        {}
-      );
-      const meses = Object.keys(totalPorMes);
-      const valoreMensal: any = Object.values(totalPorMes);
-      return { meses, valoreMensal };
+  ngOnChanges(change: SimpleChanges) {
+    if (change['contas'] && change['contas'].currentValue) {
+      this.buscaValoresDeContas(this.contas);
     }
   }
 
-  buscaContas() {
-    this.contaService.getContas().subscribe({
-      next: (data: any) => {
-        const contas: any[] = data.contas;
+  ngOnInit(): void {}
 
-        //filtra as contas
-        const constasPendente = this.filterContas(contas, 'pendente');
-        const constasPagas = this.filterContas(contas, 'pago');
-        const constasReceber = this.filterContas(contas, 'receber');
-        const constaPagar = this.filterContas(contas, 'pagar');
+  buscaValoresDeContas(contas: any) {
+    // let meses: any = [];
+    // let valorParaPagar: any = [];
+    // let valorParaReceber: any = [];
+    // for (let i = 0; i < contas.length; i++) {
+    //   meses.push(DateTime.fromISO(contas[i].vencimento).monthLong);
 
-        // Filtra os meses
-        const meses = [
-          ...constasPendente.meses,
-          ...constasPagas.meses,
-          ...constasReceber.meses,
-          ...constaPagar.meses,
-        ];
-        let mesesUnicos = [...new Set(meses)].sort(
-          (a, b) =>
-            new Date(`01 ${a} 2020`).getMonth() -
-            new Date(`01 ${b} 2020`).getMonth()
-        );
+    //   if (contas[i].tipo === 'pagar') {
+    //     valorParaPagar.push({ meses: [meses[i]], valor: [contas[i].valor] });
+    //   } else {
+    //     valorParaReceber.push({ meses: [meses[i]], valor: [contas[i].valor] });
+    //   }
+    // }
+    // Refatorar: Tentar evitar o tipo any , usar tipagem, usar loops mais simples e atuais.
 
-        // Mapeia os valores para cada mês
-        const valoresPendentesPorMes = new Map(
-          mesesUnicos.map((mes) => [mes, 0])
-        );
-        constasPendente.meses.forEach((mes: any, index: any) => {
-          valoresPendentesPorMes.set(
-            mes,
-            valoresPendentesPorMes.get(mes)! +
-              constasPendente.valoreMensal[index]
-          );
-        });
+    //Obter meses e agrupar valores por tipo
+    const meses = [
+      ...new Set(
+        contas.map(
+          (conta: any) => DateTime.fromISO(conta.vencimento).monthLong
+        ) as string
+      ),
+    ];
+    const valorParaPagar: { mes: string; valor: number }[] = [];
+    const valorParaReceber: { mes: string; valor: number }[] = [];
 
-        const valoresPagosPorMes = new Map(mesesUnicos.map((mes) => [mes, 0]));
-        constasPagas.meses.forEach((mes: any, index: number) => {
-          valoresPagosPorMes.set(
-            mes,
-            valoresPagosPorMes.get(mes)! + constasPagas.valoreMensal[index]
-          );
-        });
-
-        const valoresPagar = new Map(mesesUnicos.map((mes) => [mes, 0]));
-        constaPagar.meses.forEach((mes: any, index: number) => {
-          valoresPagar.set(
-            mes,
-            valoresPagar.get(mes)! + constaPagar.valoreMensal[index]
-          );
-        });
-
-        const valoresReceber = new Map(mesesUnicos.map((mes) => [mes, 0]));
-        constasReceber.meses.forEach((mes: any, index: number) => {
-          valoresReceber.set(
-            mes,
-            valoresReceber.get(mes)! + constasReceber.valoreMensal[index]
-          );
-        });
-
-        this.chartOptions = {
-          ...this.chartOptions,
-          series: [
-            // {
-            //   name: '$ Total de contas com status pago',
-            //   data: Array.from(valoresPagosPorMes.values()),
-            //   color: 'blue', // Adiciona os valores como dados
-            // },
-            // {
-            //   name: '$ Total de contas com status pendente',
-            //   data: Array.from(valoresPendentesPorMes.values()),
-            //   color: 'gray', // Adiciona os valores como dados
-            // },
-            {
-              name: 'Total mensal a pagar',
-              data: Array.from(valoresPagar.values()),
-              color: 'red',
-            },
-            {
-              name: 'Total mensal a receber',
-              data: Array.from(valoresReceber.values()),
-              color: 'green',
-            },
-          ],
-          chart: {
-            height: 350,
-            type: 'bar',
-            zoom: {
-              enabled: false,
-            },
-            animations: {
-              speed: 200,
-            },
-            dropShadow: {
-              enabled: false,
-              enabledOnSeries: undefined,
-              top: 0,
-              left: 0,
-              blur: 3,
-              color: '#000',
-              opacity: 0.35,
-            },
-          },
-          plotOptions: {
-            bar: {
-              horizontal: false,
-              columnWidth: '55%',
-            },
-          },
-
-          dataLabels: {
-            enabled: false,
-          },
-          stroke: {
-            show: true,
-            width: 2,
-            colors: ['transparent'],
-          },
-          title: {
-            text: 'Total de valores por mês',
-            align: 'left',
-          },
-          grid: {
-            row: {
-              colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-              opacity: 0.5,
-            },
-          },
-          xaxis: {
-            categories: mesesUnicos,
-          },
-          fill: { opacity: 1 },
-          yaxis: {
-            title: {
-              text: 'R$ (valores)',
-            },
-          },
-          tooltip: {
-            y: {
-              formatter: (val: any) => `R$ ${val}`,
-            },
-          },
-        };
-      },
-      error: (error) => {
-        console.log('Error ao buscar contas:', error);
-      },
+    contas.forEach((conta: any) => {
+      const mes = DateTime.fromISO(conta.vencimento).monthLong as string;
+      if (conta.tipo === 'pagar') {
+        valorParaPagar.push({ mes, valor: Number(conta.valor) });
+      } else {
+        valorParaReceber.push({ mes, valor: Number(conta.valor) });
+      }
     });
+
+    //Cria um mapa com os meses
+    const mesesUnicos = new Map(meses.map((mes) => [mes, 0]));
+
+    this.insereValoresNoGrafico(mesesUnicos, valorParaReceber, valorParaPagar);
+  }
+
+  insereValoresNoGrafico(
+    meses: Map<string, number>,
+    valoresReceber: { mes: string; valor: number }[],
+    valoresPagar: { mes: string; valor: number }[]
+  ) {
+    let receber = new Map(meses);
+    let pagar = new Map(meses);
+
+    // function mapeamento(map: any, valores: any) {
+    //   valores.forEach((el: any, idx: any) => {
+    //     if (map.has(...el.meses)) {
+    //       return map.set(...el.meses, map.get(...el.meses) + Number(el.valor));
+    //     } else {
+    //       return map.set(...el.meses, Number(el.valor));
+    //     }
+    //   });
+    // }
+    // mapeamento(pagar, valorPago);
+    // mapeamento(receber, valorReceber);
+    // Atualizar mapas com valores
+    const atualizarMapa = (
+      map: Map<string, number>,
+      valores: { mes: string; valor: number }[]
+    ) => {
+      valores.forEach(({ mes, valor }) => {
+        map.set(mes, (map.get(mes) || 0) + valor);
+      });
+    };
+
+    atualizarMapa(receber, valoresReceber);
+    atualizarMapa(pagar, valoresPagar);
+
+    //Obter valores
+    const valorReceber = Array.from(receber.values());
+    const valorPagar = Array.from(pagar.values());
+
+    this.configurarGrafico(meses, valorReceber, valorPagar);
+  }
+
+  configurarGrafico(
+    meses: Map<string, number>,
+    valoresReceber: number[],
+    valoresPagar: number[]
+  ) {
+    this.chartOptions = {
+      ...this.chartOptions,
+      series: [
+        {
+          name: 'Total mensal a pagar',
+          data: valoresPagar,
+          color: 'red',
+        },
+        {
+          name: 'Total mensal a receber',
+          data: valoresReceber,
+          color: 'green',
+        },
+      ],
+      chart: {
+        height: 350,
+        type: 'bar',
+        zoom: {
+          enabled: false,
+        },
+        animations: {
+          speed: 200,
+        },
+        dropShadow: {
+          enabled: false,
+          enabledOnSeries: undefined,
+          top: 0,
+          left: 0,
+          blur: 3,
+          color: '#000',
+          opacity: 0.35,
+        },
+      },
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          columnWidth: '55%',
+        },
+      },
+
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        show: true,
+        width: 2,
+        colors: ['transparent'],
+      },
+      title: {
+        text: 'Total de valores por mês',
+        align: 'left',
+      },
+      grid: {
+        row: {
+          colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+          opacity: 0.5,
+        },
+      },
+      xaxis: {
+        categories: Array.from(meses.keys()),
+      },
+      fill: { opacity: 1 },
+      yaxis: {
+        title: {
+          text: 'R$ (valores)',
+        },
+      },
+      tooltip: {
+        y: {
+          formatter: (val: any) => `R$ ${val}`,
+        },
+      },
+    };
   }
 }
