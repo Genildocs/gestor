a2654ffa79f2ca14ce54f94aefb2bd81f6cd7b35
genildocs <genildocs@gmail.com>
2025-01-28 06:12:18 -0300

    aplicação em manutenção

--- src/app/shared/pages/auth/login/login.component.ts
+++ src/app/shared/pages/auth/login/login.component.ts
@@ -9,54 +9,50 @@ import { MessageService } from 'primeng/api';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  loginForm: FormGroup;
-  loading: boolean = false;
-  isValidFormSubmitted = false;
-
-  constructor(
-    private fb: FormBuilder,
-    private authService: AuthService,
-    private router: Router,
-    private messageService: MessageService
-  ) {
-    this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(8)]],
-    });
-  }
-
-  show() {
-    this.messageService.add({
-      severity: 'error',
-      summary: 'Error de login',
-      detail: 'Email ou senha inválidos',
-    });
-  }
-
-  onSubmit() {
-    const formData = this.loginForm.value;
-    this.loading = true;
-
-    this.authService.loginUser(formData.email, formData.password).subscribe({
-      next: (response: { token: string; username: string }) => {
-        this.loading = false;
-        // const { token } = response;
-        // console.log(response);
-        // this.authService.saveToken(token);
-        this.router.navigate(['dashboard/home']);
-      },
-      error: (error) => {
-        this.loading = false;
-        this.isValidFormSubmitted = true;
-        this.show();
-        setTimeout(() => {
-          this.isValidFormSubmitted = false;
-        }, 3000);
-        console.error('Error registering user:', error);
-      },
-      complete: () => {
-        console.log('Login concluido');
-      },
-    });
-  }
+  // loginForm: FormGroup;
+  // loading: boolean = false;
+  // isValidFormSubmitted = false;
+  // constructor(
+  //   private fb: FormBuilder,
+  //   private authService: AuthService,
+  //   private router: Router,
+  //   private messageService: MessageService
+  // ) {
+  //   this.loginForm = this.fb.group({
+  //     email: ['', [Validators.required, Validators.email]],
+  //     password: ['', [Validators.required, Validators.minLength(8)]],
+  //   });
+  // }
+  // show() {
+  //   this.messageService.add({
+  //     severity: 'error',
+  //     summary: 'Error de login',
+  //     detail: 'Email ou senha inválidos',
+  //   });
+  // }
+  // onSubmit() {
+  //   const formData = this.loginForm.value;
+  //   this.loading = true;
+  //   this.authService.loginUser(formData.email, formData.password).subscribe({
+  //     next: (response: { token: string; username: string }) => {
+  //       this.loading = false;
+  //       // const { token } = response;
+  //       // console.log(response);
+  //       // this.authService.saveToken(token);
+  //       this.router.navigate(['dashboard/home']);
+  //     },
+  //     error: (error) => {
+  //       this.loading = false;
+  //       this.isValidFormSubmitted = true;
+  //       this.show();
+  //       setTimeout(() => {
+  //         this.isValidFormSubmitted = false;
+  //       }, 3000);
+  //       console.error('Error registering user:', error);
+  //     },
+  //     complete: () => {
+  //       console.log('Login concluido');
+  //     },
+  //   });
+  // }
 }
